eadd477a808102795bb2b3f3f4017f2f22447ffe
Thiago Rodrigues <taorodrigues9@gmail.com>
2020-11-09 15:57:59 -0300

    Colocando use gradeRouter abaixo do useCors.

--- app.js
+++ app.js
@@ -19,34 +19,27 @@ import { db } from './models/index.js';
   }
 })();
 
-// (async () => {
-//   try {
-//     await db.mongoose.connect(
-//       'mongodb+srv://tigo:james1991@cluster0.q4in0.mongodb.net/grades?retryWrites=true&w=majority',
-//       {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       }
-//     );
-//     console.log('Conectado no MongoDB');
-//     console.log(process.env.PORT);
-//   } catch (error) {
-//     console.log('Erro ao conectar no MongoDB');
-//   }
-// })();
-
 const app = express();
 
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
-app.use(gradeRouter);
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:3000', //colocar aqui a url do meu site
   })
 );
+// app.use((req, res, next) => {
+//   //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+//   res.header('Access-Control-Allow-Origin', '*');
+//   //Quais são os métodos que a conexão pode realizar na API
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   app.use(cors());
+//   next();
+// });
 
+app.use(gradeRouter);
 app.get('/', (req, res) => {
   res.send('API em execucao');
 });
