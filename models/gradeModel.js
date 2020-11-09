add9aa74e2b4e467d6a2f0912d5d5ffe355dede5
Thiago Rodrigues <taorodrigues9@gmail.com>
2020-11-09 16:06:12 -0300

    Alterando schema para retornar o _id como id.

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -1,28 +1,35 @@
 import mongoose from 'mongoose';
 
-const gradeSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const gradeSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    lastModified: {
+      type: Date,
+      default: Date.now(),
+      required: true,
+    },
   },
-  subject: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  value: {
-    type: String,
-    required: true,
-  },
-  lastModified: {
-    type: Date,
-    default: Date.now(),
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 const gradeModel = mongoose.model('grade', gradeSchema, 'grade');
 
